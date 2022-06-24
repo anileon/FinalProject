63ec4ae0705236f4bef8b3567610e3ba810a4775
SebastianMedinaNieto <102368512+SebastianMedinaNieto@users.noreply.github.com>
2022-06-24 18:07:07 -0300

    productos agregados

--- src/main/java/com/FINALPROJECT/MotoRider/MotoRiderApplication.java
+++ src/main/java/com/FINALPROJECT/MotoRider/MotoRiderApplication.java
@@ -52,6 +52,56 @@ public class MotoRiderApplication {
 					3,1);
 			productRepository.save(casco4);
 
+			Product guantes1 = new Product("Gloves", GenderType.FEMALE, "Leather black gloves", 100, List.of("https://i.ibb.co/3hvCvS8/97116-22-VW-F-removebg-preview.png"),
+					2, 1);
+			productRepository.save(guantes1);
+			Product guantes2 = new Product("Gloves", GenderType.FEMALE, "Leather brown gloves from india", 200, List.of("https://i.ibb.co/PDQpNy9/97117-22-VW-F-removebg-preview.png"),
+					3, 2);
+			productRepository.save(guantes2);
+			Product guantes3 = new Product("Gloves", GenderType.FEMALE, "Leather black gloves without fingers", 150, List.of("https://i.ibb.co/h19Q6DF/97118-22-VW-F-removebg-preview.png"),
+					1, 3);
+			productRepository.save(guantes3);
+			Product guantes4 = new Product("Gloves", GenderType.FEMALE, "Leather black gloves with pink lines", 500, List.of("https://i.ibb.co/BypP4Br/98131-20-VW-F-removebg-preview.png"),
+					3, 1);
+			productRepository.save(guantes4);
+
+			Product campera5 = new Product("Jacket", GenderType.MALE, "Black Harley leather jacket", 233.33, List.of("https://i.ibb.co/L1pSfFM/97015-22-VM-F-removebg-preview.png", "https://i.ibb.co/znTQSjk/97015-22-VM-B-removebg-preview.png"),
+					1, 4);
+			productRepository.save(campera5);
+			Product campera6 = new Product("Jacket", GenderType.MALE, "Brown Harley leather jacket with our flag", 420.420, List.of("https://i.ibb.co/HNvJrzM/97017-22-VM-F-removebg-preview-1.png", "https://i.ibb.co/h7tpXCf/97017-22-VM-B-removebg-preview.png"),
+					3, 5);
+			productRepository.save(campera6);
+			Product campera7 = new Product("Jacket", GenderType.MALE, "Black and Brown Harley leather jacket", 300, List.of("https://i.ibb.co/z5FkgJj/98053-19-VM-F-removebg-preview.png", "https://i.ibb.co/YBfgb2Y/98053-19-VM-B-removebg-preview.png"),
+					4, 4);
+			productRepository.save(campera7);
+			Product campera8 = new Product("Jacket", GenderType.MALE, "Orange and Black Harley Leather Jacket", 276.99, List.of("https://i.ibb.co/t3C63kw/98112-16-VM-F-removebg-preview.png", "https://i.ibb.co/j5G1dVt/98112-16-VM-B-removebg-preview.png"),
+					1, 5);
+			productRepository.save(campera8);
+
+			Product guantes5 = new Product("Gloves", GenderType.MALE, "Black harley leather gloves", 100, List.of("https://i.ibb.co/yB2bwZn/98140-22-VM-F-removebg-preview.png"),
+					2, 3);
+			productRepository.save(guantes5);
+			Product guantes6 = new Product("Gloves", GenderType.MALE, "Brown Harley leather gloves", 150, List.of("https://i.ibb.co/ZLx0kX9/98141-22-VM-F-removebg-preview.png"),
+					3, 4);
+			productRepository.save(guantes6);
+			Product guantes7 = new Product("Gloves", GenderType.MALE, "Beige Harley leather gloves", 137.5, List.of("https://i.ibb.co/b5mwJWP/98324-19-VM-F-removebg-preview.png"),
+					1, 5);
+			productRepository.save(guantes7);
+
+			Product casco5 = new Product("Helmet", GenderType.MALE, "Grey Harley race helmet", 250, List.of("https://i.ibb.co/1ZsTJjb/unknown.png", "https://i.ibb.co/8mX2vFZ/98228-18-VX-ALT02-removebg-preview.png"),
+					3, 4);
+			productRepository.save(casco5);
+			Product casco6 = new Product("Helmet", GenderType.MALE, "Green Harley retro helmet", 300, List.of("https://i.ibb.co/zhvbtnV/97173-22-VX-F-removebg-preview.png", "https://i.ibb.co/RbjxrTv/97173-22-VX-ALT05-removebg-preview.png"),
+					2, 5);
+			Product casco7 = new Product("Helmet", GenderType.MALE, "Red and Orange Harley race helmet", 275, List.of("https://i.ibb.co/3drpyHV/97176-22-VX-F-removebg-preview.png", "https://i.ibb.co/g7j1FbN/97176-22-VX-ALT03-removebg-preview.png"),
+					4, 4);
+			productRepository.save(casco6);
+			productRepository.save(casco7);
+
+
+
+
+
 
 
 			Client client1 = new Client("Augusto", "Casanova", "Augusto@gmail.com", "calle siempre viva 465", 1140325063, "hola");
