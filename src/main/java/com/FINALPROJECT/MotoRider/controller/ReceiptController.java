4efff2e5f6c105ab0f7e88b3d179ceb4d6e45571
SebastianMedinaNieto <102368512+SebastianMedinaNieto@users.noreply.github.com>
2022-06-29 12:54:55 -0300

    metodos para los recibos

--- src/main/java/com/FINALPROJECT/MotoRider/controller/ReceiptController.java
+++ src/main/java/com/FINALPROJECT/MotoRider/controller/ReceiptController.java
@@ -42,7 +42,6 @@ public class ReceiptController {
     }
 
     @Transactional
-
      @PostMapping("/comprar")
     public ResponseEntity<Object> Purchase(@RequestBody RecipeToCreateDTO recipeToCreateDTO){
 
@@ -75,11 +74,21 @@ public class ReceiptController {
              products.forEach(product -> {
                  int cantidadComprar = recipeToCreateDTO.getProducts().stream().filter(prod -> prod.getIdProducto() == product.getId()).findFirst().orElseThrow().getCantidad();
 
-                 product.setStock(product.getStock()-cantidadComprar);
-                 productService.saveProduct(product);
-                 ProductPurchaseOrder productPurchaseOrder = new ProductPurchaseOrder(product, LocalDateTime.now(), cantidadComprar, receipt);
-                 subtotalProductos.add(productPurchaseOrder.getCost());
-                 productPurchaseOrderService.saveProductPurchaseOrder(productPurchaseOrder);
+
+                 if(product.getStock() < cantidadComprar){
+                  new ResponseEntity<>("Amount can not be more than stock", HttpStatus.FORBIDDEN);
+
+
+                 } else {
+
+                     product.setStock(product.getStock()-cantidadComprar);
+                     productService.saveProduct(product);
+                     ProductPurchaseOrder productPurchaseOrder = new ProductPurchaseOrder(product, LocalDateTime.now(), cantidadComprar, receipt);
+                     subtotalProductos.add(productPurchaseOrder.getCost());
+                     productPurchaseOrderService.saveProductPurchaseOrder(productPurchaseOrder);
+                 }
+
+
 
              });
          }
@@ -89,7 +98,7 @@ public class ReceiptController {
         receipt.setTotalCost(totalProducts + totalMotos);
 
         receiptService.saveReceipt(receipt);
-         return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
 
     }
 
